0262145f3bd9f251253c65481093194f66090346
Opilane TTHK <opilane@tthk.ee>
2023-03-06 13:03:29 +0200

    Initial Commit

---
+++ Calculator(SCI)/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator_SCI_;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Calculator(SCI)/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculator_SCI_;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
